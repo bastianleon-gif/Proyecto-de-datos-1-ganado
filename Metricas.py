3ca022b0de0c23f8a68f92bf2c2bb686be438d63
Bastián León <bastian.leon@pregrado.uoh.cl>
2026-05-26 14:02:43 -0400

    Modificacion codigo metricas

--- Metricas.py
+++ Metricas.py
@@ -1,7 +1,8 @@
 from ultralytics import YOLO
-model = YOLO('best.pt')
-metrics = model.val()  # Evalúa automáticamente en el conjunto de validación
+model = YOLO('best.pt') # Considerar ruta hacia el modelo
+metrics = model.val()  # Evalúa automáticamente en el conjunto de validación. Considerar ruta hacia data.yaml
 print(f"mAP50-95: {metrics.box.map:.4f}")
 print(f"mAP50: {metrics.box.map50:.4f}")
-print(f"Precisión: {metrics.box.p:.4f}")
-print(f"Recall: {metrics.box.r:.4f}")   
+print(f"Precisión: {metrics.box.p[0]:.4f}") # Agregado [0] para extraer el valor pues es un arreglo
+print(f"Recall: {metrics.box.r[0]:.4f}") # Agregado [0] para extraer el valor pues es un arreglo
+print("="*30)
